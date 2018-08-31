40bdd6806abd20c8ca5dc3e3e0e2f2c0f62e5438
will-huynh <williamcharleshuynh@gmail.com>
2018-08-31 02:09:41 -0400

    Added method to quickly create generic GUI and check to close window after form submit.

--- simple_gui.py
+++ simple_gui.py
@@ -2,13 +2,15 @@ from tkinter import *
 import os
 import sys
 
-class SimpleGUI(object):
+class Simple_GUI(object):
 
     def __init__(self, fields):
 
         #Configuration settings for GUI window
         self.root_window = Tk()
         self.tk_pack_directions = [LEFT, RIGHT, TOP, BOTTOM]
+        self.default_button_name = 'Submit'
+        self.close_window_after_submit = True
 
         #Names of text-entry fields
         self.fields = fields
@@ -17,6 +19,26 @@ class SimpleGUI(object):
         #Results from user text-entry fields
         self.results = []
         self.submit_form = False
+
+    #Method to provide a way to close GUI within the scope of the methods of the class
+    def quit(self):
+        self.root_window.destroy()
+
+    #Method to fetch entry field data; returns field name and data (None if empty) as well as a boolean event (True) for use with other modules/scripts
+    def get_entries(self):
+        for entry in self.entries:
+            field = entry[0]
+            text = entry[1].get()
+            self.results.append([field, text])
+            self.submit_form = True
+        if self.close_window_after_submit = True:
+            self.quit()
+
+    #Method to flush results if form is submitted; useful if you want multiple form submissions
+    def flush_results(self):
+        if self.submit_form is True:
+            self.results = []
+            self.submit_form = False
 
     #Method to create user text-entry form
     def create_form(self):
@@ -29,27 +51,18 @@ class SimpleGUI(object):
             ent.pack(side=RIGHT, expand=YES, fill=X)
             self.entries.append((field, ent))
 
-    #Method to fetch entry field data; returns field name and data (None if empty) as well as a boolean event (True) for use with other modules/scripts
-    def get_entries(self):
-        for entry in self.entries:
-            field = entry[0]
-            text = entry[1].get()
-            self.results.append([field, text])
-            self.submit_form = True
-
-    #Method to flush results if form is submitted; useful if you want multiple form submissions
-    def flush_results(self):
-        if self.submit_form is True:
-            self.results = []
-            self.submit_form = False
-
     #Method to create button which fetches entry field data
-    def create_submit_button(root_window, entries, name, direction):
+    def create_submit_button(self, name, direction):
         if direction in self.tk_pack_directions:
-            button = Button(root_window, text='{}'.format(name))
+            button = Button(self.root_window, text='{}'.format(name))
             button.pack(side=direction, padx=5, pady=5)
             button.bind(<'ButtonPress-1'>, self.get_entries)
         else:
             print('Direction must be: {}'.format(self.tk_pack_directions))
 
-    #Quickly
+    #Quickly create a generic menu with text-entry fields and a submit button; provide method w/ False to prevent window from closing on completion
+    def create_gui(self, close_window):
+        self.create_form()
+        self.create_submit_button()
+        if close_window is False:
+            self.close_window_after_submit = False
